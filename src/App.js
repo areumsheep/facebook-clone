263e0159a0559d2e8304f4a4121e9fc42f84d6c2
areumsheep <s2018s10@e-mirim.hs.kr>
2020-09-24 23:14:09 +0900

    header 이미지 추가

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    // BEM naming convention
+    <div className="app">
+      <Header />
+      
+      {/* App body */}
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// 여기에 config 코드 써도 안전!
+const firebaseConfig = {
+    apiKey: "AIzaSyC_i_zqroGAucvf9y1pBI4_zepW35UwQII",
+    authDomain: "facebook-clone-26872.firebaseapp.com",
+    databaseURL: "https://facebook-clone-26872.firebaseio.com",
+    projectId: "facebook-clone-26872",
+    storageBucket: "facebook-clone-26872.appspot.com",
+    messagingSenderId: "92562347021",
+    appId: "1:92562347021:web:d272aca27a360a3413b144",
+    measurementId: "G-F2YF1L6K1Y"
+};
